8dfeed1f73f69bbac13937beb6764e49b7d17566
jvarghe <vcs.inbox@outlook.com>
2023-03-21 21:46:45 +0000

    Added comments and cleaned up 'spinner2.js'.

--- spinner2.js
+++ spinner2.js
@@ -4,25 +4,35 @@
  */
 
 
+// This function prints a crude spinner to the console. The spinner rotates for
+// one full cycle.
 const displaySpinner = function() {
 
+  // This contains spinner characters. 
   const spinners = ["|", "/", "—", "\\", "|", "/", "—", "\\", "|", ];
+
+  // The delay variable starts at 100 ms.
   let delay = 100;
 
 
+  // This loop iterates over the `spinners` array. 
   for (const element of spinners) {
   
+
     setTimeout(() => {
 
+      // It prints the spinner to screen:
       process.stdout.write("\r" + element + "  ");
 
     }, delay);
 
-    delay += 300;
 
+    // The delay is incremented by 300 ms after each run.
+    delay += 300;
   }
 
 };
 
 
+// DRIVER CODE:
 displaySpinner();
